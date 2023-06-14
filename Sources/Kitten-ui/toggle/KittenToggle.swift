d80d28f46e3a3c1ac9484dda799a1f9fafcc6cff
Sarim khan <sarimk80@gmail.com>
2023-06-14 17:34:05 +0500

    update toggle switch

--- Sources/Kitten-ui/toggle/KittenToggle.swift
+++ Sources/Kitten-ui/toggle/KittenToggle.swift
@@ -1,58 +1,85 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by sarim khan on 14/06/2023.
 //
 
 import SwiftUI
 
-public struct KittenToggle: View {
+public struct KittenToggle<Content:View>: View {
     
     @Binding var isChecked:Bool
+    @State private var circleAlignmant:Alignment = Alignment.leading
+    public var buttonStatus:ButtonStatus = ButtonStatus.primary
+    let content: Content
     
- public var body: some View {
-     ZStack(alignment: .leading){
-            
-            ToggleCapsule()
-            
-         if(isChecked) {
-             Circle()
-                 .fill(.white)
-                 .frame(width: 35,height: 35)
-                 .padding(.leading,2)
-                 .padding(.trailing,2)
-                 .transition(.slide)
-         }
-         else{
-             
-             Circle()
-                 .fill(.white)
-                 .frame(width: 35,height: 35)
-                 .padding(.leading,2)
-                 .padding(.trailing,2)
-                 .transition(.slide)
-             
-         }
-            
+    public init(isChecked: Binding<Bool>,buttonStatus: ButtonStatus = ButtonStatus.primary,@ViewBuilder  content: () -> Content) {
+        self._isChecked = isChecked
+        self.buttonStatus = buttonStatus
+        self.content = content()
+    }
+    
+    public var body: some View {
+        HStack{
+            ZStack(alignment: circleAlignmant){
+                
+                ToggleCapsule(isChecked: $isChecked,buttonStatus: buttonStatus)
+                
+                ToggleCircle(isChecked: $isChecked,buttonStatus: buttonStatus)
+                
+            }
+            .onTapGesture {
+                withAnimation(.snappy(extraBounce: 0.25)) {
+                    self.isChecked.toggle()
+                    if(self.isChecked){
+                        circleAlignmant = Alignment.trailing
+                    }else{
+                        circleAlignmant = Alignment.leading
+                    }
+                }
+            }
+            content
+                .padding(.leading,4)
         }
-     .onTapGesture {
-         withAnimation(.easeInOut(duration: 0.3)) {
-             self.isChecked.toggle()
-         }
-     }
+        
+    }
+}
+
+struct ToggleCircle: View {
+    @Binding var isChecked:Bool
+    var buttonStatus:ButtonStatus = ButtonStatus.primary
+    var body: some View {
+        ZStack{
+            Circle()
+                .fill(.white)
+                .frame(width: 35,height: 35)
+                .padding(.leading,2)
+                .padding(.trailing,2)
+            
+            if(isChecked) {
+                Image(systemName: "checkmark")
+                    .fontWeight(.heavy)
+                    .foregroundColor(buttonStatusToColor(buttonStatus: buttonStatus))
+                    .transition(.opacity.animation(.easeInOut(duration:0.4)))
+            }else{
+                EmptyView()
+            }
+        }
     }
 }
 
 struct ToggleCapsule: View {
+    @Binding var isChecked:Bool
+    var buttonStatus:ButtonStatus = ButtonStatus.primary
     var body: some View {
         ZStack{
             Capsule()
-                .stroke(.blue, lineWidth: 2)
+                .stroke(buttonStatusToColor(buttonStatus: buttonStatus), lineWidth: 2)
                 .frame(width: 70,height: 40)
             
             Capsule()
-                .fill(.blue.opacity(0.3))
+                .fill(isChecked ? buttonStatusToColor(buttonStatus: buttonStatus) : buttonStatusToColor(buttonStatus: buttonStatus).opacity(0.3))
                 .frame(width: 70,height: 40)
         }
     }
@@ -61,6 +88,8 @@ struct ToggleCapsule: View {
 struct KittenToggle_Previews: PreviewProvider {
     
     static var previews: some View {
-        KittenToggle(isChecked: .constant(false))
+        KittenToggle(isChecked: .constant(false)){
+            Text("Toggle")
+        }
     }
 }
